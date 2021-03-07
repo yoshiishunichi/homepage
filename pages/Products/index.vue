155d38d93e45d0c2af2cd83cf69732f63eb6dde3
吉井駿一 <syun_syun01@icloud.com>
2021-03-08 02:42:41 +0900

    Productsを等間隔にしたいが、、

--- pages/Products/index.vue
+++ pages/Products/index.vue
@@ -26,6 +26,21 @@
       <li class="apps-elem">
         <AppImage source="/images/Koyaku.png" link="/Products/Koyaku" />
       </li>
+      <li class="apps-elem">
+        <AppImage source="/images/Buzz.png" link="/Products/Buzz" />
+      </li>
+      <li class="apps-elem">
+        <AppImage source="/images/VS.png" link="/Products/VS" />
+      </li>
+      <li class="apps-elem">
+        <AppImage source="/images/Gochi.png" link="/Products/Gochi" />
+      </li>
+      <li class="apps-elem">
+        <AppImage source="/images/Fukidashi.png" link="/Products/Fukidashi" />
+      </li>
+      <li class="apps-elem left1">
+        <AppImage source="/images/Nisesatu.png" link="/Products/Nisesatu" />
+      </li>
     </ul>
   </div>
 </template>
@@ -34,11 +49,19 @@
 .apps-list {
   width: 95%;
   margin-top: 50px;
+  margin-left: auto;
+  margin-right: auto;
 }
 
 .apps-elem {
   width: 24.5%;
   display: inline-block;
-  text-align: center;
+  padding: 0;
+  margin: 0;
+}
+
+.left1 {
+  margin-left: 0 !important;
+  float: left;
 }
 </style>
