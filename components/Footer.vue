4668e78d3fc04cfd595c3a3a5fee188feeefcd36
吉井駿一 <syun_syun01@icloud.com>
2021-03-08 21:51:15 +0900

    Footer追加

---
+++ components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <h6>© 2021 ganja_tuber</h6>
+</template>
+
+<style lang="scss" scoped>
+h6 {
+  font-size: 15px;
+  color: white;
+  font-family: 'MS P Gothic', sans-serif;
+  text-align: center;
+  width: 100%;
+  margin-top: 60px;
+  margin-bottom: 20px;
+}
+</style>
