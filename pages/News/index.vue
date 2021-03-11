7330cfeb85684dd4a6488a57c482a96e13f41091
吉井駿一 <syun_syun01@icloud.com>
2021-03-11 23:19:16 +0900

    日付修正

--- pages/News/index.vue
+++ pages/News/index.vue
@@ -3,7 +3,7 @@
     <h4 class="page-title">News</h4>
     <NewsSection
       title="ホームページリニューアル"
-      date="2021/3/9"
+      date="2021/3/11"
       description="Nuxt.jsを使ってganja_tuberのホームページをリニューアルしました。<br><br>がんばっておしゃれな感じになったのでよかったら見てください。<br><br>今後もがんばっていきますので何卒よろしくお願いします。<br><br>from ganja_tuber"
     />
   </div>
