5d7ac2afe296b926b3d5e4f9032a2a5c306b302b
吉井駿一 <syun_syun01@icloud.com>
2021-04-27 16:40:19 +0900

    topへ戻るボタン実装

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,15 @@
     <Header ref="header" />
     <div @click="parentEvent">
       <Nuxt />
+      <transition name="button">
+        <button
+          v-show="buttonActive"
+          class="top-btn back-color"
+          @click="returnTop"
+        >
+          <font-awesome-icon :icon="['fas', 'chevron-up']" class="top-icon" />
+        </button>
+      </transition>
     </div>
     <Footer />
   </div>
@@ -15,10 +24,59 @@ export default {
   components: {
     Header,
   },
+  data() {
+    return {
+      buttonActive: false,
+      scroll: 0,
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.scrollWindow)
+  },
   methods: {
     parentEvent() {
       this.$refs.header.close()
     },
+    returnTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      })
+    },
+    scrollWindow() {
+      const top = 210
+      this.scroll = window.scrollY
+      if (top <= this.scroll) {
+        this.buttonActive = true
+      } else {
+        this.buttonActive = false
+      }
+    },
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.button-enter-active,
+.button-leave-active {
+  transition: opacity 0.5s;
+}
+.button-enter,
+.button-leave-to {
+  opacity: 0;
+}
+
+.top-btn {
+  position: fixed;
+  bottom: 51px;
+  right: 30px;
+  width: 42px;
+  height: 42px;
+  outline: none;
+  color: white;
+  border-radius: 21px;
+  cursor: pointer;
+  border: none;
+  box-shadow: 3px 8px 8px rgba(0, 0, 0, 0.6);
+}
+</style>
