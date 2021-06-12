c6155bab2c1bc1b40cf1ae118dc6bad3c72fd6e4
吉井駿一 <syun_syun01@icloud.com>
2021-06-12 18:51:54 +0900

    bioのテキスト修正

--- pages/Biography/index.vue
+++ pages/Biography/index.vue
@@ -3,7 +3,7 @@
     <h4 class="page-title">Biography</h4>
     <BioSection
       title="Yoshii Shunichi"
-      description="神戸の大学に通っています。<br>2020年の4月に容疑者画像メーカーというiOSアプリを公開して以降、精力的に趣味の悪いアプリやwebサイトを作成しています。<br>アプリの感想など、Twitterで連絡いただけると元気になります。"
+      description="神戸の大学に通っています。<br>2020年の6月に容疑者画像メーカーというiOSアプリを公開して以降、精力的に趣味の悪いアプリやwebサイトを作成しています。<br>アプリの感想など、Twitterで連絡いただけると元気になります。"
     />
   </div>
 </template>
